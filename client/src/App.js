08593e8b6b54c5d71111184ebe889df81b0db90c
Hyeon <csh-scl>
2023-12-28 13:02:25 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,9 @@ import TableHead from '@mui/material/TableHead';
 import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
-import { makeStyles, withStyles } from '@mui/styles';
+import { withStyles } from '@mui/styles';
 
-
-
-const useStyles = makeStyles((theme) => ({
+const styles = theme => ({
   root: {
     width: '100%',
     // marginTop: theme.spacing(3),
@@ -21,55 +19,51 @@ const useStyles = makeStyles((theme) => ({
   table: {
     minWidth: 1080
   }
-}));
+});
 
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://picsum.photos/id/2/200/300',
-    'name': '조성현',
-    'gender': '남자',
-    'job': '대학생',
-    'birthday': '010410'
-  },
-  {
-    'id': 2,
-    'image': 'https://www.radiokorea.com/bulletin/data/file/c_talk_entertain/644496677_h725MNu8_3b441ac283041ecef6989eeefc9ed59e.jpg',
-    'name': '유민지',
-    'gender': '여자',
-    'job': '배우',
-    'birthday': '981014'
-  },
-  {
-    'id': 3,
-    'image': 'https://postfiles.pstatic.net/MjAyMjA0MjJfMTM0/MDAxNjUwNTkyNjU2MTQ2.3bJ6jVY-izzzTell7_XF_zDMeWOnnTZ8kHB1TU0WjEQg.QJmA8cBMWwNG6J3AUvVWLU0dAkdMEnIBNxFSbFDA5iEg.JPEG.hwoarangx2/9d7691e61855bbaa680a05a719ebbaf6.jpg?type=w773',
-    'name': '장원영',
-    'gender': '여자',
-    'job': '아이돌',
-    'birthday': '040814'
+class App extends Component {
+  state = {
+    customers: ""
   }
-]
 
-const App = () => {
-  const classes = useStyles();
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
 
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableCell>번호</TableCell>
-          <TableCell>이미지</TableCell>
-          <TableCell>이름</TableCell>
-          <TableCell>생년원일</TableCell>
-          <TableCell>성별</TableCell>
-          <TableCell>직업</TableCell>
-        </TableHead>
-        <TableBody>
-          {customers.map(c => <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { customers } = this.state;
+
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년원일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers ? this.state.customers.map(c => {
+            return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)}
+            ) :""}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
-export default withStyles(useStyles)(App);
+export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -4,10 +4,35 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id': 1,
+            'image': 'https://picsum.photos/id/2/200/300',
+            'name': '조성현',
+            'gender': '남자',
+            'job': '대학생',
+            'birthday': '010410'
+        },
+        {
+            'id': 2,
+            'image': 'https://www.radiokorea.com/bulletin/data/file/c_talk_entertain/644496677_h725MNu8_3b441ac283041ecef6989eeefc9ed59e.jpg',
+            'name': '유민지',
+            'gender': '여자',
+            'job': '배우',
+            'birthday': '981014'
+        },
+        {
+            'id': 3,
+            'image': 'https://postfiles.pstatic.net/MjAyMjA0MjJfMTM0/MDAxNjUwNTkyNjU2MTQ2.3bJ6jVY-izzzTell7_XF_zDMeWOnnTZ8kHB1TU0WjEQg.QJmA8cBMWwNG6J3AUvVWLU0dAkdMEnIBNxFSbFDA5iEg.JPEG.hwoarangx2/9d7691e61855bbaa680a05a719ebbaf6.jpg?type=w773',
+            'name': '장원영',
+            'gender': '여자',
+            'job': '아이돌',
+            'birthday': '040814'
+        }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
