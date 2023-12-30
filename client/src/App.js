e6e8b3ad103c0947825f8a4385348b2a0ee95bd3
Hyeon <csh-scl>
2023-12-30 18:49:03 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- client/src/App.js
+++ client/src/App.js
@@ -14,33 +14,40 @@ import { withStyles } from '@mui/styles';
 const styles = theme => ({
   root: {
     width: '100%',
-    // marginTop: theme.spacing(3),
+    marginTop: '24px',
     overflowX: 'scroll'
   },
   table: {
     minWidth: 1080
   },
   progress: {
-    //margin: theme.spacing.unit * 2
+    progress: {
+      margin: '16px',  // 16px = 2 * 8px (theme.spacing(2)와 동일)
+    },
   }
 });
 
 class App extends Component {
   state = {
     customers: "",
-    completed : 0
+    completed: 0
   }
 
   componentDidMount() {
     this.timer = setInterval(this.progress, 20);
     this.callApi()
-      .then(res => this.setState({customers: res}))
+      .then(res => {
+        this.setState({ customers: res });
+        clearInterval(this.timer);
+      })
+
       .catch(err => console.log(err));
   }
 
-  progress = () =>{
-    const {completed} = this.state;
-    this.setState({completed: completed >=100 ? 0: completed + 1});
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
   }
 
   callApi = async () => {
@@ -68,11 +75,12 @@ class App extends Component {
           </TableHead>
           <TableBody>
             {this.state.customers ? this.state.customers.map(c => {
-            return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)}
-            ) :""}
+              return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)
+            }
+            ) : ""}
             <TableRow>
               <TableCell colSpan="6" align="center">
-                <CircularProgress className={classes.progress} variant="indeterminate" value={this.state.completed}/>
+                <CircularProgress className={classes.progress} variant="indeterminate" value={this.state.completed} />
               </TableCell>
             </TableRow>
           </TableBody>
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://picsum.photos/id/2/200/300',
-            'name': '조성현',
-            'gender': '남자',
-            'job': '대학생',
-            'birthday': '010410'
-        },
-        {
-            'id': 2,
-            'image': 'https://www.radiokorea.com/bulletin/data/file/c_talk_entertain/644496677_h725MNu8_3b441ac283041ecef6989eeefc9ed59e.jpg',
-            'name': '유민지',
-            'gender': '여자',
-            'job': '배우',
-            'birthday': '981014'
-        },
-        {
-            'id': 3,
-            'image': 'https://postfiles.pstatic.net/MjAyMjA0MjJfMTM0/MDAxNjUwNTkyNjU2MTQ2.3bJ6jVY-izzzTell7_XF_zDMeWOnnTZ8kHB1TU0WjEQg.QJmA8cBMWwNG6J3AUvVWLU0dAkdMEnIBNxFSbFDA5iEg.JPEG.hwoarangx2/9d7691e61855bbaa680a05a719ebbaf6.jpg?type=w773',
-            'name': '장원영',
-            'gender': '여자',
-            'job': '아이돌',
-            'birthday': '040814'
-        }
-    ]);
+    connection.query(
+        "SELECT * FROM customer",
+        (err, rows, fields) => {
+            res.send(rows);
+        } 
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
